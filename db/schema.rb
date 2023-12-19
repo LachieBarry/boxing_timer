80773959819287cd4e3f2dca62f3ea033ca406b9
Lachlan Barry <lachlan.j.barry@gmail.com>
2023-12-19 19:11:38 +1100

    round model created

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_12_19_053521) do
+ActiveRecord::Schema[7.1].define(version: 2023_12_19_074432) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "rounds", force: :cascade do |t|
+    t.integer "work_time"
+    t.integer "rest_time"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "workouts", force: :cascade do |t|
     t.string "name"
